2ad2b7f4ff1cec5f14afa6a42a9712e093cf65b2
joao <lucajovihome@gmail.com>
2025-12-05 08:35:43 -0300

    first commit

---
+++ FCAutoSave(Duarte).bat
@@ -0,0 +1,7 @@
+git config --global user.name "joao"
+git config --global user.email lucajovihome@gmail.com
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DuartedeSousa/teste.git
+git push -u origin main
