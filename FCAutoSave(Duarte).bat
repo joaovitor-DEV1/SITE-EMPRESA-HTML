13f05bc43c6704c1ef40d0e11aa37f111443f60d
joao <lucajovihome@gmail.com>
2025-12-12 10:17:23 -0300

    turd commit

--- FCAutoSave(Duarte).bat
+++ FCAutoSave(Duarte).bat
@@ -1,7 +1,7 @@
 git config --global user.name "joao"
 git config --global user.email lucajovihome@gmail.com
 git add .
-git commit -m "second commit"
+git commit -m "turd commit"
 git branch -M main
 git remote add origin https://github.com/joaovitor-DEV1/SITE-EMPRESA-HTML.git
 git push -u origin main
