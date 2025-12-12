a78f88eb899fdbadd7b2b41b61fa063d960f57b5
joao <lucajovihome@gmail.com>
2025-12-12 10:51:12 -0300

    algum commit

--- FCAutoSave(Duarte).bat
+++ FCAutoSave(Duarte).bat
@@ -1,7 +1,7 @@
 git config --global user.name "joao"
 git config --global user.email lucajovihome@gmail.com
 git add .
-git commit -m "forth commit"
+git commit -m "algum commit"
 git branch -M main
 git remote add origin https://github.com/joaovitor-DEV1/SITE-EMPRESA-HTML.git
 git push -u origin main
